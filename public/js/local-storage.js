66b8b063ce82064b25816cbbd9ccf0592610021d
Lucas Lima <77409981+lucassli@users.noreply.github.com>
2021-12-21 00:58:30 -0300

    Delete public/js directory

--- public/js/local-storage.js
+++
@@ -1,8 +0,0 @@
-let btnAdicionarTarefas = document.querySelector('#btn-adicionar').addEventListener('click', adcTarefas);
-let btnArquivarTarefas = document.querySelector('.btn-card-arquivar').addEventListener('click', arqTarefas);
-let btnExcluirTarefas = document.querySelector('.btn-card-excluir').addEventListener('click', excTarefas);
-
-function adcTarefas(){
-    let tarefas = [
-        {}
-    ]
